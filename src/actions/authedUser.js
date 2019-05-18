ccbd667e5edd646affd0f300c2f795192a915c1b
Kevin Ziegler <kevin.ziegler@bigcommerce.com>
2019-05-18 18:38:20 -0500

    Add redux handling for logout

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,13 +1,14 @@
 import { _getUsers } from '../util/_DATA';
 export const SET_AUTHED_USER = 'SET_AUTHED_USER';
 export const LOAD_USERS = 'LOAD_USERS';
+export const LOGOUT = 'LOGOUT';
 
 export const setAuthedUser = (id) => ({type: SET_AUTHED_USER, id});
 export const loadUsers = (users) => ({type: LOAD_USERS, users});
+export const logout = () => ({ type: LOGOUT });
 
 export const handleLoadUsers = () => {
     return dispatch => {
-        // FIXME: Add error handling
         _getUsers().then(users => dispatch(loadUsers(users)));
     };
 };
--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,12 +1,13 @@
-import { SET_AUTHED_USER, LOAD_USERS } from '../actions/authedUser';
+import { SET_AUTHED_USER, LOAD_USERS, LOGOUT } from '../actions/authedUser';
 
 export default function authedUser (state = {}, action) {
     switch (action.type) {
     case SET_AUTHED_USER:
-        // FIXME: Should this be the full user?
         return { ...state, userId: action.id };
     case LOAD_USERS:
         return { ...state, availableUsers: action.users };
+    case LOGOUT:
+        return { ...state, userId: null };
     default:
         return state;
     }
